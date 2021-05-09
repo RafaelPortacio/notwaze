b47b210cdebf33e4c8b2d5506b2f91d912a9dad4
Daniel Csillag <dccsillag@gmail.com>
2021-05-09 13:05:15 -0300

    Add code in main.cpp to load the graph and print basic stats

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 #include "graph.hpp"
+#include "io.hpp"
 
 
 int main() {
-    std::cout << "hello" << std::endl;
+    const Graph graph = load_graph_from_json_file("rj_graph_database.json");
+
+    std::cout << "n_nodes = " << graph.n_nodes() << std::endl;
+    std::cout << "n_edges = " << graph.n_edges() << std::endl;
 }
