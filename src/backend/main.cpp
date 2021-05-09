7abc9bd20ef4d082449f9edcad5753608c0b6e9c
Daniel Csillag <dccsillag@gmail.com>
2021-05-09 14:27:21 -0300

    Add most restinio boilerplate for the REST API

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+#include <restinio/all.hpp>
+
 #include "graph.hpp"
 #include "io.hpp"
+
+
+void serve_rest_api(const std::string& address, const unsigned int& port) {
+    auto router = std::make_unique<restinio::router::express_router_t<>>();
+    router->http_get("/sayHello",
+                     [](const auto& req, const auto& params) {
+                         return req->create_response()
+                             .set_body("Hello World!")
+                             .done();
+                     });
+    router->non_matched_request_handler([](auto req) {
+        return req->create_response(restinio::status_not_found()).connection_close().done();
+    });
+
+    struct my_traits_t : public restinio::default_single_thread_traits_t {
+        using request_handler_t = restinio::router::express_router_t<>;
+    };
+
+    restinio::run(restinio::on_this_thread<my_traits_t>()
+                  .address(address)
+                  .port(port)
+                  .request_handler(std::move(router)));
+}
 
 
 int main() {
@@ -9,4 +34,6 @@ int main() {
 
     std::cout << "n_nodes = " << graph.n_nodes() << std::endl;
     std::cout << "n_edges = " << graph.n_edges() << std::endl;
+
+    serve_rest_api("localhost", 8080);
 }
