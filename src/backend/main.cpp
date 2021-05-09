fecd7467260b963b99ea8f5b9625dad5cf64ef6d
Wellington Silva <wellington.71319@gmail.com>
2021-05-09 18:24:35 -0300

    Update 09/05

--- src/backend/main.cpp
+++ src/backend/main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>    // std::min_element, std::max_element
+#include <optional>
 
 #include "graph.hpp"
 
+using namespace std;
 
-// void shortest_path_dijkstra (...) {
-//     ...
-// }
+
+vector<id_t> shortest_path_dijkstra (const Graph& G, const id_t& s, const id_t& t) {
+    unordered_map<id_t, double> d;
+    unordered_map<id_t, id_t> p;
+    unordered_set<id_t> F;
+
+    for (auto iter = G.cbegin_nodes(); iter != G.cend_nodes(); ++iter) {
+        d.insert({iter->first, INFINITY});
+        p.insert({iter->first, 0});
+        F.insert(iter->first);
+    }
+
+    d[s] = 0;
+
+    while (!F.empty()) {
+        id_t mv = *min_element(F.cbegin(), F.cend(), [&d](auto l ,auto r){ return d[l] < d[r]; });
+        
+        for 
+    } 
+}
 
 int main() {
-    std::cout << "hello" << std::endl;
+    cout << "hello" << endl;
 }
