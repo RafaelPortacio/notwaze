3b4a625faf05b089aa46d926cfe6d7b399cbdd9f
Wellington Silva <wellington.71319@gmail.com>
2021-05-15 21:59:09 -0300

    New dijkstra

--- src/backend/shortest-path.cpp
+++ src/backend/shortest-path.cpp
@@ -3,42 +3,50 @@
 #include <bits/stdc++.h>
 #include <string>
 #include <algorithm>    // std::min_element, std::max_element
+#include <queue>
 
 #include "graph.hpp"
 
 using namespace std;
 
 
-pair<vector<id_t>, eta_t> shortest_path_dijkstra (const Graph& G, const id_t& s, const id_t& t) {
-    unordered_map<id_t, eta_t> d;
-    unordered_map<id_t, id_t> p;
-    unordered_set<id_t> F;
+vector<id_t> shortest_path_dijkstra (const Graph& graph,
+                                     const id_t& start_point,
+                                     const id_t& end_point) {
+    priority_queue<pair <id_t, eta_t>> frontier;
+    unordered_map<id_t, id_t> came_from;
+    unordered_map<id_t, eta_t> cost_so_far;
 
-    for (auto iter = G.cbegin_nodes(); iter != G.cend_nodes(); ++iter) {
-        d.insert({iter->first, INFINITY});
-        p.insert({iter->first, 0});
-        F.insert(iter->first);
-    }
+    frontier.push({start_point, 0});
+    cost_so_far.insert({start_point, 0});
 
-    d[s] = 0;
+    while (!frontier.empty()) {
+        pair<id_t, eta_t> current = frontier.top();
 
-    while (!F.empty()) {
-        id_t mv = *min_element(F.cbegin(), F.cend(), [&d](auto l ,auto r){ return d[l] < d[r]; });
-        
-        for (auto iter = G.cbegin_outedges(mv); iter != G.cend_outedges(mv); ++iter) {
-            if (d[iter->first] < d[mv] + iter->second.eta) {
-                d[iter->first] = d[mv] + iter->second.eta;
-                p[iter->first] = mv;
+        if (current.first == end_point) {
+            break;
+        }
+
+        for (auto iter = graph.cbegin_outedges(current.first);
+             iter != graph.cend_outedges(current.first);
+              ++iter) {
+            eta_t new_cost = cost_so_far.at(current.first) + iter->second.eta;
+
+            if (!cost_so_far.count(iter->first) || new_cost < cost_so_far.at(iter->first)) {
+                cost_so_far[iter->first] = new_cost;
+                eta_t priority = new_cost;
+                frontier.push({iter->first, priority});
+                came_from[iter->first] = current.first;
             }
         }
-        F.erase(mv);
     }
 
-    vector<id_t> path {t};
-    id_t current = t;
-    while (current != s) {
-        current = p[current];
+    vector<id_t> path {end_point};
+    id_t current = end_point;
+    while (current != start_point) {
+        current = came_from.at(current);
         path.push_back(current);
     }
-    return {path, d[t]};
+    
+    return path;
 }
