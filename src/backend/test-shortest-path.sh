ccc6b86d592677fa0af808695a1a08098b0e1746
Daniel Csillag <dccsillag@gmail.com>
2021-05-22 16:47:12 -0300

    Update shortest path test script

--- src/backend/test-shortest-path.sh
+++ src/backend/test-shortest-path.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/sh -e
+
+GET_SHORTEST_PATH=$(find . -name get-shortest-path)
 
 INPUTS_DIR="$1"
 METHOD="$2"
+
+endnl () {
+    [ -f "$1" ] && [ -s "$1" ] && [ -z "$(tail -c 1 "$1")" ]
+}
 
 trim() {
     sed 's/ *\(.\+\) */\1/'
@@ -9,26 +15,30 @@ trim() {
 
 for input_graph in "$INPUTS_DIR"/*.json
 do
+    paths_file="${input_graph%.json}.txt"
+    endnl "$paths_file" || { echo >> "$paths_file"; }
+
     sed -i 's/ //g' "$input_graph"
     while read -r correct_path
     do
         correct_path=$(echo "$correct_path" | trim)
         starting_point="${correct_path%% *}"
-        ending_point="${correct_path## *}"
-        cpp_shortest_path=$(get-shortest-path "$METHOD" "$input_graph" "$starting_point" "$ending_point" | trim)
+        ending_point="${correct_path##* }"
+        cpp_shortest_path=$("$GET_SHORTEST_PATH" "$METHOD" "$input_graph" "$starting_point" "$ending_point" | trim)
         if [ "$correct_path" = "$cpp_shortest_path" ]
         then
             echo "Succeeded"
         else
-            echo 'Failed!'
-            echo 'Inputs:'
+            echo "$(tput bold)Failed!$(tput sgr0)"
+            echo "Inputs:"
             echo "  method = $METHOD"
             echo "  graph = $input_graph"
             echo "  starting_point = $starting_point"
             echo "  ending_point = $ending_point"
-            echo 'Outputs:'
+            echo "Outputs:"
             echo "  correct: $correct_path"
             echo "  ours:    $cpp_shortest_path"
+            exit 1
         fi
-    done < "${input_graph%.json}.txt"
+    done < "$paths_file"
 done
