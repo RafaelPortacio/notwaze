6f82c627820d18eee223ef509859098c7aa3847d
Daniel Csillag <dccsillag@gmail.com>
2021-05-22 16:27:40 -0300

    Add script to test shortest path implementations

---
+++ src/backend/test-shortest-path.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+INPUTS_DIR="$1"
+METHOD="$2"
+
+trim() {
+    sed 's/ *\(.\+\) */\1/'
+}
+
+for input_graph in "$INPUTS_DIR"/*.json
+do
+    while read -r correct_path
+    do
+        correct_path=$(echo "$correct_path" | trim)
+        starting_point="${correct_path%% *}"
+        ending_point="${correct_path## *}"
+        cpp_shortest_path=$(get-shortest-path "$METHOD" "$input_graph" "$starting_point" "$ending_point" | trim)
+        if [ "$correct_path" = "$cpp_shortest_path" ]
+        then
+            echo "Succeeded"
+        else
+            echo 'Failed!'
+            echo 'Inputs:'
+            echo "  method = $METHOD"
+            echo "  graph = $input_graph"
+            echo "  starting_point = $starting_point"
+            echo "  ending_point = $ending_point"
+            echo 'Outputs:'
+            echo "  correct: $correct_path"
+            echo "  ours:    $cpp_shortest_path"
+        fi
+    done < "${input_graph%.json}.txt"
+done
