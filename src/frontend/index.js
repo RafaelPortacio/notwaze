ec0a6dd4b14c3cc8ea70b7a21af5e8ca89c13956
Daniel Csillag <dccsillag@gmail.com>
2021-05-23 12:50:12 -0300

    Add try-catch for when geocoding fails in frontend

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -10,6 +10,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Draw shortest paths, if there are any
 var params = (new URL(document.location)).searchParams;
 
+class GeocodingError extends Error {
+    constructor(place_str) {
+        super("Geocoding failed for '" + place_str + "'");
+        this.name = "GeocodingError";
+    }
+}
+
 async function geocode_latlong(place_str) {
     var url = "https://nominatim.openstreetmap.org/search?"
         + "street=" + encodeURI(place_str)
@@ -20,50 +27,62 @@ async function geocode_latlong(place_str) {
     var response = await response.json();
     console.log(response);
 
+    if (response.length == 0) {
+        await Promise.reject(new GeocodingError(place_str));
+    }
+
     var place = response[0];
 
     return [place.lat, place.lon];
 }
 
 async function find_and_draw_path(starting_point_str, destination_str) {
-    var [start_point_latitude, start_point_longitude] = await geocode_latlong(starting_point_str);
-    var [end_point_latitude, end_point_longitude]     = await geocode_latlong(destination_str);
+    try {
+        var [start_point_latitude, start_point_longitude] = await geocode_latlong(starting_point_str);
+        var [end_point_latitude, end_point_longitude]     = await geocode_latlong(destination_str);
 
-    var url = "http://localhost:8080/shortestPath?"
-        + "startPointLat=" + encodeURI(start_point_latitude)
-        + "&startPointLong=" + encodeURI(start_point_longitude)
-        + "&endPointLat=" + encodeURI(end_point_latitude)
-        + "&endPointLong=" + encodeURI(end_point_longitude);
-    var data = await fetch(url);
-    var data = await data.json();
+        var url = "http://localhost:8080/shortestPath?"
+            + "startPointLat=" + encodeURI(start_point_latitude)
+            + "&startPointLong=" + encodeURI(start_point_longitude)
+            + "&endPointLat=" + encodeURI(end_point_latitude)
+            + "&endPointLong=" + encodeURI(end_point_longitude);
+        var data = await fetch(url);
+        var data = await data.json();
 
-    var starting_point = data["shortest-paths"][0]["path"][0];
-    var ending_point =   data["shortest-paths"][0]["path"][data["shortest-paths"][0]["path"].length-1];
+        var starting_point = data["shortest-paths"][0]["path"][0];
+        var ending_point =   data["shortest-paths"][0]["path"][data["shortest-paths"][0]["path"].length-1];
 
-    // Add markers
-    L.marker([starting_point.latitude, starting_point.longitude]).addTo(leaflet_map);
-    L.marker([ending_point.  latitude, ending_point.  longitude]).addTo(leaflet_map);
+        // Add markers
+        L.marker([starting_point.latitude, starting_point.longitude]).addTo(leaflet_map);
+        L.marker([ending_point.  latitude, ending_point.  longitude]).addTo(leaflet_map);
 
-    // Draw lines for indicating paths
-    let extents = data["shortest-paths"].map(function(path) {
-        let latlongs = path["path"].map(x => [x.latitude, x.longitude]);
-        let polyline = L.polyline(latlongs, {
-            color: "blue",
-            weight: 7,
-            opacity: 0.5,
-        }).addTo(leaflet_map).bindPopup("ETA: " + humanizeDuration(1000 * path["eta"]), {
-            autoPan: false,
+        // Draw lines for indicating paths
+        let extents = data["shortest-paths"].map(function(path) {
+            let latlongs = path["path"].map(x => [x.latitude, x.longitude]);
+            let polyline = L.polyline(latlongs, {
+                color: "blue",
+                weight: 7,
+                opacity: 0.5,
+            }).addTo(leaflet_map).bindPopup("ETA: " + humanizeDuration(1000 * path["eta"]), {
+                autoPan: false,
+            });
+            polyline.on("mouseover", function(ev) {
+                polyline.openPopup();
+                polyline.getPopup().setLatLng(ev.latlng);
+            });
+            polyline.on("mouseout",  function(ev) {
+                polyline.closePopup();
+            });
+            return polyline.getBounds();
         });
-        polyline.on("mouseover", function(ev) {
-            polyline.openPopup();
-            polyline.getPopup().setLatLng(ev.latlng);
-        });
-        polyline.on("mouseout",  function(ev) {
-            polyline.closePopup();
-        });
-        return polyline.getBounds();
-    });
-    leaflet_map.fitBounds(extents.reduce((acc, x) => acc.extend(x)));
+        leaflet_map.fitBounds(extents.reduce((acc, x) => acc.extend(x)));
+    } catch (err) {
+        if (err instanceof GeocodingError) {
+            alert(err.message);
+        } else {
+            throw err;
+        }
+    }
 }
 
 if (params.has("starting-point") && params.has("destination")) {
