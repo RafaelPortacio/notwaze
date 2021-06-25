f7f4a32fa00de7340247ec987a74b12b22917609
Wellington Silva <wellington.71319@gmail.com>
2021-06-25 15:19:29 -0300

    initial cli

--- src/backend/cli.cpp
+++ src/backend/cli.cpp
@@ -7,44 +7,90 @@
 #include "shortest-path.hpp"
 
 
-int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        std::cerr << "Usage: " << argv[0] << " <METHOD> <GRAPH_FILE> <START_NODE> <END_NODE>" << std::endl;
-        return 2;
-    }
+int main() {
+    int algoritm = -1;
+    std::string starting_point_str_x;
+    std::string starting_point_str_y;
+    std::string ending_point_str_x;
+    std::string ending_point_str_y;
 
-    std::string method(argv[1]);
-    std::string graph_path(argv[2]);
-    node_id starting_point = std::stoi(argv[3]);
-    node_id ending_point = std::stoi(argv[4]);
 
+    std::cout << "... Loading index";
+    
     // Load graph
-    const Graph graph = load_graph_from_json_file(graph_path);
+    const Graph graph = load_graph_from_json_file("rj_graph_database.json");
 
-    // Eval shortest path
-    std::optional<std::vector<node_id>> maybe_path;
-    if (method == "dijkstra") {
-        maybe_path = shortest_path_dijkstra(graph, starting_point, ending_point, get_weight_length);
-    } else if (method == "astar-euclidean") {
-        maybe_path = shortest_path_astar(graph, starting_point, ending_point, euclidean_heuristic, get_weight_length);
-    } else if (method == "astar-manhattan") {
-        maybe_path = shortest_path_astar(graph, starting_point, ending_point, manhattan_heuristic, get_weight_length);
-    } else {
-        std::cerr << "Bad shortest path method: " << method << std::endl;
-        return 1;
+    std::cout << ", pronto!" << std::endl;
+    std::cout << "Which algoritm to use?" << std::endl;
+    while (algoritm != 0 && algoritm != 1 && algoritm != 2) {
+        std::cout << "[0] Dijkstra" << std::endl 
+                  << "[1] A* with euclidean heuristic" << std::endl
+                  << "[2] A* with manhattan heuristic" << std::endl;
+        std::cin >> algoritm;
     }
 
-    if (!maybe_path) {
-        std::cout << std::endl;
-        return 0;
-    }
-    const std::vector<node_id>& path = *maybe_path;
+    std::cout << "Foda-se!" << std::endl;
 
-    assert(path.front() == starting_point);
-    assert(path.back()  == ending_point);
-
-    for (const auto& node_id : path) {
-        std::cout << node_id << " ";
-    }
+    std::cout << "Enter the origin: ";
+    std::cin >> starting_point_str_x;
+    std::cin >> starting_point_str_y;
     std::cout << std::endl;
+    std::cout << "Enter the destination: ";
+    std::cin >> ending_point_str_x;
+    std::cin >> ending_point_str_y;
+    std::cout << std::endl;
+
+    starting_point_str_x = starting_point_str_x.substr(0, starting_point_str_x.size()-1);
+    ending_point_str_x = ending_point_str_x.substr(0, ending_point_str_x.size()-1);
+
+    long double starting_point_x = std::stold(starting_point_str_x);
+    long double starting_point_y = std::stold(starting_point_str_y);
+    long double ending_point_x = std::stold(ending_point_str_x);
+    long double ending_point_y = std::stold(ending_point_str_y);
+
+
+    std::cout << starting_point_x + starting_point_y << std::endl;
+
+    // node_id starting_point = std::stoi(starting_point_str);
+    // node_id ending_point = std::stoi(ending_point_str);
+
+    return 0;
+
+    // if (argc != 4) {
+    //     std::cerr << "Usage: " << argv[0] << " <METHOD> <START_NODE> <END_NODE>" << std::endl;
+    //     return 2;
+    // }
+
+    // std::string method(argv[1]);
+    // std::string graph_path(argv[2]);
+    // node_id starting_point = std::stoi(argv[3]);
+    // node_id ending_point = std::stoi(argv[4]);
+
+
+    // // Eval shortest path
+    // std::optional<std::vector<node_id>> maybe_path;
+    // if (method == "dijkstra") {
+    //     maybe_path = shortest_path_dijkstra(graph, starting_point, ending_point, get_weight_length);
+    // } else if (method == "astar-euclidean") {
+    //     maybe_path = shortest_path_astar(graph, starting_point, ending_point, euclidean_heuristic, get_weight_length);
+    // } else if (method == "astar-manhattan") {
+    //     maybe_path = shortest_path_astar(graph, starting_point, ending_point, manhattan_heuristic, get_weight_length);
+    // } else {
+    //     std::cerr << "Bad shortest path method: " << method << std::endl;
+    //     return 1;
+    // }
+
+    // if (!maybe_path) {
+    //     std::cout << std::endl;
+    //     return 0;
+    // }
+    // const std::vector<node_id>& path = *maybe_path;
+
+    // assert(path.front() == starting_point);
+    // assert(path.back()  == ending_point);
+
+    // for (const auto& node_id : path) {
+    //     std::cout << node_id << " ";
+    // }
+    // std::cout << std::endl;
 }
